481a03078d7b8552ca6bdd82a0b36258b332f3eb
koryruno <koryruno@yandex.ru>
2024-04-03 00:47:26 +0300

    added: test director

---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorageTest.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.storage.director;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.yandex.practicum.filmorate.model.Director;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@JdbcTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class DirectorDbStorageTest {
+    private final JdbcTemplate jdbcTemplate;
+    private DirectorDbStorage directorStorage;
+
+    @BeforeEach
+    void init() {
+        directorStorage = new DirectorDbStorage(jdbcTemplate);
+    }
+
+    @Test
+    public void testAddDirector() {
+        assertThatCode(() -> {
+            Director director = new Director(1L, "MegaFamousDirector");
+            directorStorage.add(director);
+        }).doesNotThrowAnyException();
+    }
+
+    // testUpdateDirector
+
+    // testGetDirector
+
+    // testGetAllDirectors
+
+    // testDeleteDirector
+
+    // testAddDirectorsToFilm
+
+    // testGetFilmDirectors
+}
