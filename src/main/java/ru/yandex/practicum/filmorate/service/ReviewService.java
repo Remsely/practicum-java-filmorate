5741f2e119f47d3787a86992551abfbf23aa9429
SidyakinV <144915259+SidyakinV@users.noreply.github.com>
2024-04-06 16:16:09 +0300

    Add reviews (#16)
    feat: добавить отзывы на фильмы

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,162 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.review.ReviewDbStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class ReviewService {
+
+    private final ReviewDbStorage reviewStorage;
+    private final UserStorage userStorage;
+    private final FilmStorage filmStorage;
+
+
+    @Autowired
+    public ReviewService(
+            ReviewDbStorage reviewStorage,
+            @Qualifier("userDbStorage") UserStorage userStorage,
+            @Qualifier("filmDbStorage") FilmStorage filmStorage) {
+        this.reviewStorage = reviewStorage;
+        this.userStorage = userStorage;
+        this.filmStorage = filmStorage;
+    }
+
+    public Review addReview(Review review) {
+        validation(review);
+        checkUserExists(review.getUserId());
+        checkFilmExists(review.getFilmId());
+        Optional<Review> savedReview = reviewStorage.add(review);
+        if (savedReview.isEmpty()) {
+            throwReviewNotExists(review.getId());
+        }
+        log.info("Отзыв добавлен: {}", savedReview);
+        return savedReview.get();
+    }
+
+    // При обновлении отзыва проверка на валидность фильма и пользователя не производится,
+    // т.к. пользователь может редактировать только свой отзыв на тот фильм, на который
+    // был написан отзыв
+    public Review updateReview(Review review) {
+        validation(review);
+        Optional<Review> savedReview = reviewStorage.update(review);
+        if (savedReview.isEmpty()) {
+            throwReviewNotExists(review.getId());
+        }
+        log.info("Отзыв обновлен: {}", savedReview);
+        return savedReview.get();
+    }
+
+    public void deleteReview(long id) {
+        if (!reviewStorage.delete(id)) {
+            throwReviewNotExists(id);
+        }
+        log.info("Отзыв с id {} удален", id);
+    }
+
+    public Review getReview(long id) {
+        Optional<Review> review = reviewStorage.getReview(id);
+        if (review.isEmpty()) {
+            throwReviewNotExists(id);
+        }
+        log.info("Получен отзыв с id {}. Review: {}", id, review);
+        return review.get();
+    }
+
+    public List<Review> getAllReviews(int count) {
+        List<Review> reviews = reviewStorage.getAllReviews(count);
+        log.info("Получен список отзывов на все фильмы (count {}). List : {}", count, reviews);
+        return reviews;
+    }
+
+    public List<Review> getFilmReviews(long filmId, int count) {
+        List<Review> reviews = reviewStorage.getFilmReviews(filmId, count);
+        log.info("Получен список отзывов на фильм с id {} (count {})", filmId, count);
+        return reviews;
+    }
+
+    public void addLike(long id, long userId) {
+        checkReviewExists(id);
+        checkUserExists(userId);
+        reviewStorage.addLike(id, userId);
+        log.info("Пользователь с userId {} поставил лайк фильму с filmId {}", userId, id);
+    }
+
+    public void addDislike(long id, long userId) {
+        checkReviewExists(id);
+        checkUserExists(userId);
+        reviewStorage.addDislike(id, userId);
+        log.info("Пользователь с userId {} поставил дизлайк фильму с filmId {}", userId, id);
+    }
+
+    public void removeLike(long id, long userId) {
+        // Здесь эти проверки не особо то и нужны, но пусть будут
+        checkReviewExists(id);
+        checkUserExists(userId);
+        reviewStorage.removeLike(id, userId);
+        log.info("Пользователь с userId {} удалил лайк к фильму с filmId {}", userId, id);
+    }
+
+    public void removeDislike(long id, long userId) {
+        // Здесь эти проверки не особо то и нужны, но пусть будут
+        checkReviewExists(id);
+        checkUserExists(userId);
+        reviewStorage.removeDislike(id, userId);
+        log.info("Пользователь с userId {} удалил дизлайк к фильму с filmId {}", userId, id);
+    }
+
+    private void checkUserExists(long userId) {
+        if (userStorage.notContainUser(userId)) {
+            throw new EntityNotFoundException(
+                    new ErrorResponse("User id", String.format("Не найден пользователь с ID: %d.", userId))
+            );
+        }
+    }
+
+    private void checkFilmExists(long filmId) {
+        if (filmStorage.notContainFilm(filmId)) {
+            throw new EntityNotFoundException(
+                    new ErrorResponse("Film id", String.format("Не найден фильм с ID: %d.", filmId))
+            );
+        }
+    }
+
+    private void checkReviewExists(long reviewId) {
+        if (reviewStorage.notContainReview(reviewId)) {
+            throwReviewNotExists(reviewId);
+        }
+    }
+
+    private void throwReviewNotExists(long reviewId) {
+        throw new EntityNotFoundException(
+                new ErrorResponse("Review id", String.format("Не найден отзыв с ID: %d.", reviewId)));
+    }
+
+    private void validation(Review review) {
+        if (review.getContent() == null || review.getContent().isBlank()) {
+            throw new ValidationException("Не заполнено содержание отзыва");
+        }
+        if (review.getUserId() == null) {
+            throw new ValidationException("Не указан код пользователя");
+        }
+        if (review.getFilmId() == null) {
+            throw new ValidationException("Не указан код фильма");
+        }
+        if (review.getIsPositive() == null) {
+            throw new ValidationException("Не указан тип отзыва");
+        }
+    }
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -0,0 +1,148 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class ReviewDbStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public Optional<Review> add(Review review) {
+        String sqlQuery =
+                "INSERT INTO review (content, user_id, film_id, is_positive) " +
+                        "VALUES (?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, new String[]{"review_id"});
+            stmt.setString(1, review.getContent());
+            stmt.setLong(2, review.getUserId());
+            stmt.setLong(3, review.getFilmId());
+            stmt.setBoolean(4, review.getIsPositive());
+            return stmt;
+        }, keyHolder);
+
+        long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
+
+        return getReview(id);
+    }
+
+    public Optional<Review> update(Review review) {
+        String sqlQuery =
+                "UPDATE review SET content = ?, is_positive = ? WHERE review_id = ?";
+        jdbcTemplate.update(sqlQuery,
+                review.getContent(),
+                review.getIsPositive(),
+                review.getId());
+        return getReview(review.getId());
+    }
+
+    public boolean delete(long id) {
+        String sqlQuery =
+                "DELETE FROM review WHERE review_id = ?";
+        return jdbcTemplate.update(sqlQuery, id) > 0;
+    }
+
+    public Optional<Review> getReview(long id) {
+        String sqlQuery = getBaseCommand() +
+                "WHERE review_id = ?";
+        SqlRowSet rows = jdbcTemplate.queryForRowSet(sqlQuery, id);
+        if (rows.next()) {
+            return Optional.of(mapRowToReview(rows));
+        }
+        return Optional.empty();
+    }
+
+    public List<Review> getAllReviews(int count) {
+        String sqlQuery = getBaseCommand() +
+                "ORDER BY useful DESC \n " +
+                "LIMIT ? ";
+        SqlRowSet rows = jdbcTemplate.queryForRowSet(sqlQuery, count);
+
+        List<Review> list = new ArrayList<>();
+        while (rows.next()) {
+            list.add(mapRowToReview(rows));
+        }
+        return list;
+    }
+
+    public List<Review> getFilmReviews(long filmId, int count) {
+        String sqlQuery = getBaseCommand() +
+                "WHERE film_id = ? \n " +
+                "ORDER BY useful DESC \n " +
+                "LIMIT ? ";
+        SqlRowSet rows = jdbcTemplate.queryForRowSet(sqlQuery, filmId, count);
+
+        List<Review> list = new ArrayList<>();
+        while (rows.next()) {
+            list.add(mapRowToReview(rows));
+        }
+        return list;
+    }
+
+    public void addLike(long id, long userId) {
+        String sqlQuery =
+                "MERGE INTO like_review (user_id, review_id, usefull) VALUES (?, ?, true)";
+        jdbcTemplate.update(sqlQuery, userId, id);
+    }
+
+    public void addDislike(long id, long userId) {
+        String sqlQuery =
+                "MERGE INTO like_review (user_id, review_id, usefull) VALUES (?, ?, false)";
+        jdbcTemplate.update(sqlQuery, userId, id);
+    }
+
+    public void removeLike(long id, long userId) {
+        String sqlQuery =
+                "DELETE FROM like_review WHERE user_id = ? AND review_id = ?";
+        jdbcTemplate.update(sqlQuery, userId, id);
+    }
+
+    public void removeDislike(long id, long userId) {
+        String sqlQuery =
+                "DELETE FROM like_review WHERE user_id = ? AND review_id = ?";
+        jdbcTemplate.update(sqlQuery, userId, id);
+    }
+
+    public boolean notContainReview(long reviewId) {
+        String sqlQuery = "SELECT review_id FROM review WHERE review_id = ?";
+        SqlRowSet rows = jdbcTemplate.queryForRowSet(sqlQuery, reviewId);
+        return !rows.next();
+    }
+
+    private String getBaseCommand() {
+        return
+                "SELECT \n " +
+                "  review_id, content, user_id, film_id, is_positive, \n " +
+                "  IFNULL((SELECT sum(CASE usefull WHEN true THEN 1 ELSE -1 END) \n " +
+                "          FROM like_review \n " +
+                "          WHERE review_id = r.review_id), \n " +
+                        " 0) AS useful \n " +
+                "FROM review as r \n ";
+    }
+
+    private Review mapRowToReview(SqlRowSet rs) {
+        return Review.builder()
+                .id(rs.getLong("review_id"))
+                .content(rs.getString("content"))
+                .userId(rs.getLong("user_id"))
+                .filmId(rs.getLong("film_id"))
+                .isPositive(rs.getBoolean("is_positive"))
+                .useful(rs.getLong("useful"))
+                .build();
+    }
+
+}
