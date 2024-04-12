fe24472caa91b4708759d64a591bbc5f56d7c055
Roman Prokopchuk <115027949+Remsely@users.noreply.github.com>
2024-04-12 10:52:51 +0300

    fix feed (#29)
    * feat: добавить кеширование индексов типов событий и оперций при первом запросе индекса в БД
    
    * fix: удалить лишние комментарии

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedDbStorage.java
@@ -7,22 +7,29 @@ import ru.yandex.practicum.filmorate.model.feed.FeedEventType;
 import ru.yandex.practicum.filmorate.model.feed.FeedOperation;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
 public class FeedDbStorage implements FeedStorage {
     private final JdbcTemplate jdbcTemplate;
+    private final Map<FeedOperation, Long> feedOperationIndexes = new HashMap<>();
+    private final Map<FeedEventType, Long> feedEventTypeIndexes = new HashMap<>();
 
     @Override
     public void add(long userId, long entityId, FeedEventType eventType, FeedOperation operation) {
         final String sqlQuery = "INSERT INTO feed (user_id, entity_id, type_id, operation_id, time) " +
                 "VALUES (?, ?, ?, ?, ?)";
 
+        Long eventTypeIndex = feedEventTypeIndexes.computeIfAbsent(eventType, this::getEventTypeIndex);
+        Long eventOperation = feedOperationIndexes.computeIfAbsent(operation, this::getOperationIndex);
+
         jdbcTemplate.update(sqlQuery,
                 userId,
                 entityId,
-                this.getEventTypeIndex(eventType),
-                this.getOperationIndex(operation),
+                eventTypeIndex,
+                eventOperation,
                 LocalDateTime.now()
         );
     }
